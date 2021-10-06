0ed13c96c694f3399f5920539f6559a2abccb70a
adrnmlgrto <adrn.mlgrto@gmail.com>
2021-10-06 15:22:22 +0800

    Updated Registration.dart with background image of PARIS.jpg, Added First & Last Name TextFields.

--- lib/registration.dart
+++ lib/registration.dart
@@ -12,66 +12,90 @@ class _RegistrationPageState extends State<RegistrationPage> {
   @override
   Widget build(BuildContext context) => Scaffold(
         resizeToAvoidBottomInset: false,
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(30.0, 0, 30.0, 30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Sign Up',
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 30.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  fillColor: Colors.white54,
-                  filled: true,
-                  labelText: 'Email',
-                ),
-              ),
-              SizedBox(height: 15.0),
-              TextFormField(
-                obscureText: _passwordVisible,
-                decoration: InputDecoration(
-                  fillColor: Colors.white54,
-                  filled: true,
-                  labelText: 'Password',
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _passwordVisible
-                          ? Icons.visibility
-                          : Icons.visibility_off,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _passwordVisible = !_passwordVisible;
-                      });
-                    },
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/PARIS.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 0, 30.0, 30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              SizedBox(height: 40.0),
-              Row(
-                children: [
-                  Expanded(
-                      child: ElevatedButton(
-                          onPressed: () {}, child: Text('REGISTER NOW'))),
-                ],
-              ),
-              SizedBox(height: 10.0),
-              TextButton(
-                style: TextButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 16),
+                SizedBox(height: 30.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white54,
+                    filled: true,
+                    labelText: 'First Name',
+                  ),
                 ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('Already have an account? Login.'),
-              ),
-            ],
+                SizedBox(height: 15.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white54,
+                    filled: true,
+                    labelText: 'Last Name',
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white54,
+                    filled: true,
+                    labelText: 'Email',
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                TextFormField(
+                  obscureText: _passwordVisible,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white54,
+                    filled: true,
+                    labelText: 'Password',
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40.0),
+                Row(
+                  children: [
+                    Expanded(
+                        child: ElevatedButton(
+                            onPressed: () {}, child: Text('REGISTER NOW'))),
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 16),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Already have an account? Login.'),
+                ),
+              ],
+            ),
           ),
         ),
       );
