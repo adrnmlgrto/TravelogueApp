7be85e294138890dcd3f5a3e3eee9f1b202f440c
adrnmlgrto <adrn.mlgrto@gmail.com>
2021-10-06 23:52:27 +0800

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
-import 'login.dart';
+import 'package:myflutter/login.dart';
 
 void main() => runApp(MaterialApp(
       title: 'Travelogue',
