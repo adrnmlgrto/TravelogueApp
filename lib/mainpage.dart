70c2a7266b30df91af66f57796686d72f9f77ff2
adrnmlgrto <adrn.mlgrto@gmail.com>
2021-10-10 17:44:37 +0800

    Added profile page in mainpage.dart and added dependencies for assets.

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:myflutter/homepage.dart';
 import 'package:myflutter/bookAirport.dart';
 import 'package:myflutter/bookHotel.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:myflutter/login.dart';
 
 class MainPage extends StatefulWidget {
   final String fname;
@@ -16,18 +18,6 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-// class UserDetails {
-//   String? fname;
-//   String? lname;
-//   String? email;
-
-//   UserDetails(String fname, String lname, String email) {
-//     this.fname = fname;
-//     this.lname = lname;
-//     this.email = email;
-//   }
-// }
-
 class _MainPageState extends State<MainPage> {
   int currentIndex = 0;
   final pageScreens = [
@@ -39,23 +29,103 @@ class _MainPageState extends State<MainPage> {
     ),
   ];
 
-  // Future createProfile(int index) async {
-  //   String f_name = widget.fname;
-  //   String l_name = widget.lname;
-  //   String e_mail = widget.email;
-
-  //   UserDetails person = UserDetails(f_name, l_name, e_mail);
-
-  //   print('${person.email}');
-  // }
+  Widget showScreen(int index) {
+    if (index == 0) {
+      return HomePage();
+    } else if (index == 1) {
+      return BookingAir();
+    } else if (index == 2) {
+      return BookingHotel();
+    }
+    return Padding(
+      padding: EdgeInsets.fromLTRB(30, 50, 30, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage: CachedNetworkImageProvider(
+                'https://i.ibb.co/mt2zFtZ/Profile.jpg',
+              ),
+            ),
+          ),
+          Divider(
+            height: 70,
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Icon(Icons.person_outline),
+              SizedBox(width: 5),
+              Text(
+                'NAME',
+                style: TextStyle(
+                  letterSpacing: 2,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Text(
+            '${widget.fname} ${widget.lname}',
+            style: TextStyle(
+              color: Colors.amber[900],
+              letterSpacing: 1,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 30),
+          Row(
+            children: [
+              Icon(
+                Icons.email_outlined,
+                color: Colors.black,
+              ),
+              SizedBox(width: 5),
+              Text(
+                'EMAIL',
+                style: TextStyle(
+                  letterSpacing: 2,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Text(
+            '${widget.email}',
+            style: TextStyle(
+              color: Colors.amber[900],
+              letterSpacing: 1,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 130),
+          Row(
+            children: [
+              Expanded(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage()));
+                      },
+                      child: Text('LOGOUT'))),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    // print(widget.fname);
-    // print(widget.lname);
-    // print(widget.email);
     return Scaffold(
-      body: pageScreens[currentIndex],
+      // body: pageScreens[currentIndex],
+      body: showScreen(currentIndex),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         iconSize: 30,
